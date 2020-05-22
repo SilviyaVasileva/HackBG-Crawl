5f5ceddd165aa8538a260ad6518520ab1fd474b7
SilviyaVasileva <60818785+SilviyaVasileva@users.noreply.github.com>
2020-05-22 20:37:19 +0300

    parallel version

--- db.py
+++ db.py
@@ -6,6 +6,7 @@ from sqlalchemy.orm import sessionmaker
 class Database():
     def __init__(self):
         self.Base = declarative_base()
-        self.engine = create_engine("sqlite:///URL.db")
+        self.engine = create_engine('sqlite:///URL.db', connect_args={'timeout': 15}) 
         self.sessionM = sessionmaker(bind=self.engine)
         self.session = self.sessionM()
+        self.session.autoflush = True
